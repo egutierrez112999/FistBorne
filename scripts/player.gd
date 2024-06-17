4e2f003760dca4790d1460353f29403c17d77279
egutierrez112999 <egutierrez112999@gmail.com>
2024-06-17 17:25:31 -0600

    Multiplayer works on local host. Player Camera Issues

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,14 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var melee_collision_shape = $Area2D/CollisionShape2D2
+@onready var player_cam = $Camera2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+	#player_cam.enabled = false#is_multiplayer_authority()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("melee_attack"):
@@ -16,36 +21,37 @@ func _process(_delta):
 		melee_collision_shape.disabled = true
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if is_multiplayer_authority():
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Handle jump.
+		# Handle jump.
 		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input directio: -1,0,1
-	var direction = Input.get_axis("move_left", "move_right")
-	#flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+		# Get the input directio: -1,0,1
+		var direction = Input.get_axis("move_left", "move_right")
+		#flip the sprite
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 	
-	#play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+		#play animations
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			animated_sprite.play("jump")
 	
-	#apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#apply movement
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
