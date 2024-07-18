4848888b47643732310e7d6af8d20ac39a5ede95
egutierrez112999 <egutierrez112999@gmail.com>
2024-07-18 14:00:21 -0600

    Sprite changes made

--- scripts/GlobalText.gd
+++ scripts/GlobalText.gd
@@ -12,6 +12,9 @@ func format_player_data():
 		formatted_str = formatted_str + "health - " + str(player_deets[p].health) + ", "
 		formatted_str = formatted_str + "M.R. - " + str(player_deets[p].metal_reserves) + "\n"
 	return formatted_str
+	
+func _ready():
+	UpdateGameData.rpc()
 
 
 @rpc("any_peer","call_local","unreliable") func UpdateGameData():
